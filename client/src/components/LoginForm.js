04a54c7618422e8485b5f0fd7d0c3a3eecbee359
Dustin Honeck <honeck_34@hotmail.com>
2023-05-04 23:45:56 -0500

    Add Material UI to login and signup form and disable index.css

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,6 +1,6 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
+import { Alert, Button, TextField } from '@mui/material';
 
 // Refactor to switch to GraphQL login backend
 import { useMutation } from "@apollo/client";
@@ -50,42 +50,39 @@ const LoginForm = () => {
 
   return (
     <>
-      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+      <form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        {showAlert && <Alert variant='standard' severity='error' color='error' className='mb-3'>
           Something went wrong with your login credentials!
-        </Alert>
-        <Form.Group className='mb-3'>
-          <Form.Label htmlFor='email'>Email</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Your email'
+        </Alert>}
+        <div className='mb-3'>
+          <TextField
+            label='Email'
             name='email'
             onChange={handleInputChange}
             value={userFormData.email}
             required
+            style={{width:"100%"}}
           />
-          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
-        </Form.Group>
+        </div>
 
-        <Form.Group className='mb-3'>
-          <Form.Label htmlFor='password'>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Your password'
+        <div className='mb-3'>
+          <TextField
+            label='Password'
             name='password'
+            type='password'
             onChange={handleInputChange}
             value={userFormData.password}
             required
+            style={{width:"100%"}}
           />
-          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
-        </Form.Group>
+        </div>
         <Button
           disabled={!(userFormData.email && userFormData.password)}
           type='submit'
           variant='success'>
           Submit
         </Button>
-      </Form>
+      </form>
     </>
   );
 };
